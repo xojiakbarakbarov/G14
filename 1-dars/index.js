a110381f5039e7b32ba3717af79c1352e47bf9a7
xojiakbarakbarov <xakbarov508@gmail.com>
2023-07-08 12:19:36 +0500

    data types

---
+++ 1-dars/index.js
@@ -0,0 +1,57 @@
+//! Java scriptda natijani  3 xil korinishda olsa bo'ladi
+
+//? 1 - HTMl console orqali
+//? 2 - node  js orqali
+//? 3 - online complier orqali
+
+//! Java scriptda data types 8 turga bo'linadi
+
+//? 1 - number
+// let a=10;
+// let b=20;
+// console.log(a+b);
+
+//? 2 - bigInt
+
+// let a=20020220200202020202020022020n;
+// let b=7n;
+// console.log(a+b);
+
+//? 3 - string
+ // stringni 3 turda elon qilish mumkin
+
+//  let a="webbrain academy";
+//  let b='webbrain academy';
+//  let c=`webbrain academy ${a+b}`;
+
+//? 4 - boolean
+ // rostlikka tekshiradi true or false
+
+ //? 5 - null 
+ 
+ // null  o'zi bor ammo ichi bo'sh 
+
+ //? 6 - undifined 
+ 
+ // undefined  o'zi yoq  narsa
+
+ //? 7 - symbol
+  
+ // symboll faqat bitta id yaratish uchun ishlatiladi
+
+ //? 8 - objcet ==> object , array , function
+
+//? objcet ==> key va value dan tashkil topgan
+// let a = {
+//     name : "hojiakbar",
+//     surname : "akbarov"
+// }
+
+//? array
+// let a=[1,2,3,34,44," ",44]
+
+//? funcktion
+//  let a = yourName()=>{
+
+//  }
+ 
