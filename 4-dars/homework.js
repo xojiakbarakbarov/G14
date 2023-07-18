49c674e534c7f2c24e0919c5bfdefce4132598ab
xojiakbarakbarov <xakbarov508@gmail.com>
2023-07-18 15:52:14 +0500

    loops

---
+++ 4-dars/homework.js
@@ -0,0 +1,88 @@
+//? for1
+// function myfunction(k,n){
+//     for(i=0;i<n;i++){
+//         console.log(k);
+//     }
+// }
+// myfunction(7,2)
+
+//? for2
+// function myfunction(a,b){
+//     for(i=a;i<=b;i++){
+//         console.log(i);
+//     }
+// }
+// myfunction(3,6)
+
+//? for2
+// function myfunction(a,b){
+//     for(i=a+1;i<b;i++){
+//         console.log(i);
+//     }
+// }
+// myfunction(3,6)
+
+//? for4
+// function myfunction(k){
+//     for(i=0;i<=10;i++){
+//         console.log(i*k," so'm");
+//     }
+// }
+// myfunction(2000)
+
+//? for5
+// function myfunction(k){
+//     for(i=0.1;i<=1;i=i+0.1){
+//         console.log(k*i);
+//     }
+// }
+// myfunction(20000)
+
+//? for6
+// function myfunction(k){
+//     for(i=1;i<=2;i=i+0.2){
+//         console.log(k*i);
+//     }
+// }
+// myfunction(20000)
+
+//? for7
+// function myfunction(a,b){
+//     let sum=0;
+//     for(i=a;i<=b;i++){
+//         sum+=i;
+//     }
+// console.log(sum);
+// }
+// myfunction(2,5)
+
+//? for8
+// function myfunction(a, b) {
+//   let increase = 1;
+//   for (i = a; i <= b; i++) {
+//     increase = increase * i;
+//   }
+//   console.log(increase);
+// }
+// myfunction(2, 5);
+
+
+//? for9
+// function myfunction(a,b){
+//     let sum=0;
+//     for(i=a;i<=b;i++){
+//         sum+=Math.pow(i,2);
+//     }
+// console.log(sum);
+// }
+// myfunction(2,5)
+
+//? for10
+// function myfunction(n){
+//     let sum=0;
+//     for(i=1;i<=n;i++){
+//         sum+=1/i;
+//     }
+// console.log(sum);
+// }
+// myfunction(3)
---
+++ 4-dars/index.js
@@ -0,0 +1,31 @@
+//! for loop
+// for(i=0;i<100;i++){
+//     console.log(i);
+// }
+
+// let a=2;
+// let b=7;
+// for (let i=0;i<b;i++){
+//     console.log(a);
+// }
+
+// for(let i=0;i<10;i++){   //?1- for 1 marta aylanganda ikkinchi for toliq aylanadi 
+// for(let j=0;j<5;j++){    //?1- for 1 marta aylansa bu for to'liq aylandai
+//     console.log(j);
+// }
+// console.log("----------");
+// }
+
+// for(let i=2;i<=10;i++){
+//     for(let j=1;j<=10;j++){
+//         console.log(i,"x",j,"=",i*j)
+//     }
+//     console.log("                 ");
+// }
+
+//! while 
+// let a=10
+// while(a>0){
+//     console.log(a);
+//     a--;
+// }
