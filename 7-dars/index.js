0fb2696325c5df82f8edfbafc3954e57fc213788
xojiakbarakbarov <xakbarov508@gmail.com>
2023-07-23 16:14:30 +0500

    object basic

---
+++ 7-dars/index.js
@@ -0,0 +1,28 @@
+// let obj={
+//     height:10,
+//     width:5,
+//     getPerimetr(){
+//         return  2*(this.height+this.width);
+//     } ,
+//     getArea(){
+// return this.height*this.width;
+//     }
+// }
+
+// console.log(obj.getPerimetr());
+// console.log(obj.getArea());
+
+
+// let obj={
+//     radius:5,
+//     pi:3.14,
+//     getCircumfreance(){
+//         return 2*this.radius*this.pi;
+//     },
+//     getArea(){
+// return this.pi*(this.radius**2);
+//     }
+// }
+
+// console.log(obj.getCircumfreance());
+// console.log(obj.getArea());
