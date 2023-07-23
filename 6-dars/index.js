3c40793503ee2f60820a5635a94b589b63db11a3
xojiakbarakbarov <xakbarov508@gmail.com>
2023-07-23 13:17:12 +0500

    function

---
+++ 6-dars/index.js
@@ -0,0 +1,72 @@
+
+
+
+
+//! Function 
+
+
+//? function 3 turga bolinadi 
+
+//? 1  function decloration
+
+// function myFunction(){
+//     console.log("salom");
+// }
+// myFunction()
+
+
+//? 2  function expertion
+
+// const myFunction=function(){
+//     console.log("salom");
+// }
+// myFunction()
+
+//? 3 arrow function 
+
+// const myFunction=()=>{
+//     console.log("salom");
+// }
+// myFunction()
+
+//?  a va b son beriladi a ning b darajasini toping 
+
+// const numPow=(a,b)=>{
+// return Math.pow(a,b);
+// }
+// console.log(numPow(2,4));
+
+//? ikkita son berilgan bu sonlarning kattasini topuvchi dastur tuzing
+
+// function num(a,b){
+//     if(a>b) return a;
+//     else return b;
+// }
+// console.log(num(2,3));
+
+//? a va b (a>b) son kiritiladi shu sonlar orasidagi juft sonlarni ekranga choqaruvchi dastur tuzing 
+
+// const number=function(a,b){
+// for(i=b;i<=a;i++){
+//     if(i%2===0){
+//         console.log(i);
+//     }
+// }
+// }
+// number(10,3)
+
+// const tub=()=>{
+   
+//     for(i=1;i<100;i++){
+//         res=0;
+//         for(j=1;j<=i;j++){
+//             if (i%j===0){
+//                 res++;
+//             }
+//         }
+//         if(res<=2 && res!=1){
+//             console.log(i);
+//         }
+//     }
+// }
+// tub()
