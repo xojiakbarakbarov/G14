2be78c7819460ddc40129654abee7ffac451542f
xojiakbarakbarov <xakbarov508@gmail.com>
2023-08-29 10:59:08 +0500

    Prototaype and proto

---
+++ 22/index.js
@@ -0,0 +1,38 @@
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//? removeOdds
+
+// const removeOdds = (array) => {
+//   let newArray = array.filter((a) => a % 2 != 1);
+//   return newArray;
+// };
+// Array.prototype.removeOdds = removeOdds;
+
+// console.log(array.removeOdds(array));
+
+
+//?removeEvents
+
+// const  removeEvents=(array)=>{
+// let newArray=array.filter((a)=>a%2!=0)
+// return newArray
+// }
+// Array.prototype.removeEvents=removeEvents
+// console.log(array.removeEvents(array));
+
+
+
+//? getMinimum
+
+// const getMinimum=(array)=>{
+//     let minimum=array[0]
+// for(let i=0;i<array.length;i++){
+//     if(minimum>array[i]){
+//         minimum=array[i]
+//     }
+// }
+// return minimum
+// }
+// Array.prototype.getMinimum=getMinimum
+
+// console.log(array.getMinimum(array));
